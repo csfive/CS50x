982fd5501150afc000c216def5f61281fd2f1445
Mancuoj <1337381768@qq.com>
2022-06-03 02:04:46 +0800

    feat(Week5): 增加speller题解

--- Week5/speller/dictionary.c
+++ Week5/speller/dictionary.c
@@ -3,6 +3,15 @@
 #include <ctype.h>
 #include <stdbool.h>
 
+// fopen, fclose, fscanf
+#include <stdio.h>
+// malloc, free
+#include <stdlib.h>
+// strcpy
+#include <string.h>
+// strcasecmp
+#include <strings.h>
+
 #include "dictionary.h"
 
 // Represents a node in a hash table
@@ -10,19 +19,29 @@ typedef struct node
 {
     char word[LENGTH + 1];
     struct node *next;
-}
-node;
+} node;
 
 // TODO: Choose number of buckets in hash table
 const unsigned int N = 26;
 
 // Hash table
 node *table[N];
+int dict_size = 0;
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
+    // TODO: Check the spelling
+    int num = hash(word);
+    node *n = table[num];
+    while (n != NULL)
+    {
+        if (strcasecmp(word, n->word) == 0)
+        {
+            return true;
+        }
+        n = n->next;
+    }
     return false;
 }
 
@@ -36,20 +55,56 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    // TODO: Load all the data into hash table
+    FILE *dict = fopen(dictionary, "r");
+    if (dict == NULL)
+    {
+        return false;
+    }
+
+    char word[LENGTH + 1];
+    while (fscanf(dict, "%s", word) != EOF)
+    {
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return false;
+        }
+        strcpy(n->word, word);
+        int num = hash(word);
+        n->next = table[num];
+        table[num] = n;
+        dict_size++;
+    }
+
+    fclose(dict);
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    return dict_size;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
     // TODO
+    for (int i = 0; i < N; i++)
+    {
+        node *n = table[i];
+        while (n != NULL)
+        {
+            node *tmp = n;
+            n = n->next;
+            free(tmp);
+        }
+        if (i == N - 1 && n == NULL)
+        {
+            return true;
+        }
+    }
     return false;
 }
