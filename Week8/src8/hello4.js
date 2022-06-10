7023bd9fb43c890e78b032e705b3af7b1a79429e
Mancuoj <1337381768@qq.com>
2022-06-10 19:52:52 +0800

    feat: 删除无用代码

--- Week8/src8/hello4.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('form').addEventListener('submit', function(e) {
-        alert('hello, ' + document.querySelector('#name').value);
-        e.preventDefault();
-    });
-});
