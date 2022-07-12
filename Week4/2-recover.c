fec4be02b137122dc4623c4e21558d73a660214f
Kartone <estarpro1022@163.com>
2022-07-12 11:12:42 +0800

    简单修改

--- Week4/2-recover.c
+++ Week4/2-recover.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
     char filename[8];
 
     // Repeat to read 512 bytes to a buffer
-    while (fread(&buffer, BLOCK_SIZE, 1, input))
+    while (fread(buffer, sizeof(BYTE), 512, input))
     {
         // Check start of new JPEG
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
@@ -38,15 +38,14 @@ int main(int argc, char *argv[])
             }
 
             // Generate JPEG file
-            sprintf(filename, "%03i.jpg", cnt);
+            sprintf(filename, "%03i.jpg", cnt++);
             output = fopen(filename, "w");
-            cnt++;
         }
 
         // Already found JPEG
         if (cnt)
         {
-            fwrite(&buffer, BLOCK_SIZE, 1, output);
+            fwrite(buffer, sizeof(BYTE), 512, output);
         }
     }
 
